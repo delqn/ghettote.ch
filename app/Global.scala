54ea140f8b5c0bb01b09c8fdda1e05ae8b4339a0
Delyan Raychev <delyanr@gmail.com>
2015-05-23 15:46:55 -0700

    Globals.scala

--- app/Global.scala
+++ app/Global.scala
@@ -8,3 +8,13 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 object Global extends WithFilters() {
 
 }
+import play.api._
+import play.api.db.DB
+import org.squeryl._
+import org.squeryl.adapters.PostgreSqlAdapter
+
+object Global extends GlobalSettings {
+  override def onStart(app: Application) {
+    SessionFactory.concreteFactory = Some(() => Session.create(DB.getConnection()(app), new PostgreSqlAdapter))
+  }
+}
