3f3413ae74f2ef8c1682aee0b711bd2484a18c7c
Delyan Raychev <delyanr@gmail.com>
2016-04-02 20:40:41 -0700

    This should be broken but it is not

--- app/models/Persistence.scala
+++ app/models/Persistence.scala
@@ -1,17 +1,7 @@
 package models
 
-import play.api.db._
-import play.api.Play.current
-
 import org.squeryl.PrimitiveTypeMode._
-import org.squeryl._
 import org.squeryl.Schema
-
-case class User(id: Long,
-                email: String,
-                password: String,
-                fullname: String,
-                isadmin: Boolean) extends KeyedEntity[Long]
 
 object Database extends Schema {
   val usersTable = table[User]("users")
@@ -21,32 +11,3 @@ object Database extends Schema {
   }
 }
 
-object User {
-
-  // returns existing user by email address, or null if not exists
-  def apply(email: String): User = {
-    inTransaction {
-      findByEmailQ(email).headOption.getOrElse(null)
-    }
-  }
-
-  def allQ: Query[User] = from(Database.usersTable) {
-    user => select(user)
-  }
-
-  def findByEmailQ(email: String): Query[User] = from(allQ) {
-    user => where(user.email === email).select(user)
-  }
-
-  def findAll(): List[User] = {
-    inTransaction {
-      allQ.toList
-    }
-  }
-
-  def create(user: User) {
-    inTransaction {
-      Database.usersTable.insert(user)
-    }
-  }
-}
